7ef2fee578dcf657eea5acdf7ec1db7734e32c63
robi12issv <robi.bejenar@gmail.com>
2024-03-06 12:39:02 +0200

    Update

--- features/steps/comparison_steps.py
+++ features/steps/comparison_steps.py
@@ -3,18 +3,18 @@ from behave import given, when, then
 from pages.products_page import ProductsPage
 
 
-# @given('I am on the products page')
-# def step_impl(context):
-#     context.page = ProductsPage()
-#     context.page.get_products(context.config.userdata['product_name'])
-#
-#
-# @when('I verify the number of the products')
-# def step_impl(context):
-#     context.page.products_number()
-#
-#
-# @then('I verify if there are incorrect products displayed')
-# def step_impl(context):
-#     context.page.products_type()
+@given('I am on the products page')
+def step_impl(context):
+    context.page = ProductsPage()
+    context.page.get_products(context.config.userdata['product_name'])
+
+
+@when('I verify the number of the products')
+def step_impl(context):
+    context.page.products_number()
+
+
+@then('I verify if there are incorrect products displayed')
+def step_impl(context):
+    context.page.products_type()
 
